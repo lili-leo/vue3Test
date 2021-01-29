3e83a42a9bf1f5b5854eabe93098f5cf6defbb35
lili <liwei@oureman.com>
2021-01-29 21:42:31 +0800

    sasasas

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-01-29 20:43:04
+ * @LastEditTime: 2021-01-29 21:38:18
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \test\src\views\About.vue
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an ab out page</h1>
   </div>
 </template>
